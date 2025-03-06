d6aa3d21056d605a4a70265d3fd31e177421eae4
Faisal Husain <faisalhusain1320@gmail.com>
2025-03-06 13:17:38 +0530

    fix :  small css fix

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,24 +1,33 @@
 import { Button } from "@/components/ui/button";
 import { signout } from "@/lib/auth-actions";
-import {  MessagesSquare } from "lucide-react";
+import { HelpCircleIcon, MessagesSquare } from "lucide-react";
 
 const Navbar = () => {
     return (
         <nav className="flex items-center justify-between container mx-auto p-2 h-14">
             <div className="flex items-center justify-center  gap-1.5">
-                <div className="flex items-center justify-center">
-                <MessagesSquare className="size-5"/>
+                <div className="flex items-center  justify-center">
+                    <MessagesSquare className="size-6" />
                 </div>
-         <div> Chats </div>
+                <div className="font-semibold text-xl"> chats </div>
             </div>
+            <div className="flex items-center gap-2">
             <Button
-            variant={"ghost"}
+                variant={"outline"}
+                
+            >
+            <HelpCircleIcon/>    <span>Help </span>
+            </Button>
+            <Button
+                variant={"outline"}
                 onClick={() => {
                     signout();
                 }}
             >
                 Log out
             </Button>
+            </div>
+            
         </nav>
     );
 }
