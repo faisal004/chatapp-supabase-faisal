ee35087634d54a3af79130e4035dc9a51606d0e8
Faisal Husain <faisalhusain1320@gmail.com>
2025-03-07 08:48:58 +0530

    fix : online users will be seen

--- app/(dashboard)/_components/users-sidebar.tsx
+++ app/(dashboard)/_components/users-sidebar.tsx
@@ -5,6 +5,7 @@ import { Card } from "@/components/ui/card";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { useOpenStore } from "@/store/new-chat";
 import { CheckCheck } from "lucide-react";
+import { useOnlineUsersStore } from "@/store/user-online-store";
 
 type Profile = Tables<"profiles">;
 
@@ -15,8 +16,8 @@ interface SidebarProps {
 
 const UsersSidebar = ({ users, startChat }: SidebarProps) => {
   const [selectedUserId, setSelectedUserId] = useState<string | null>(null);
-  const {  setOpen } = useOpenStore();
-
+  const { setOpen } = useOpenStore();
+  const { onlineUsers } = useOnlineUsersStore()
   const handleUserClick = (userId: string) => {
     setSelectedUserId(userId);
     startChat(userId);
@@ -26,43 +27,50 @@ const UsersSidebar = ({ users, startChat }: SidebarProps) => {
   return (
     <div className="h-[calc(100vh-58px)] bg-gray-100 ">
       <ScrollArea className=" h-[calc(100vh-65px)] ">
-    
+
         {users.map((u) => (
           <Card
             key={u.id}
             className={`p-3 overflow-hidden rounded-[5px] border-0 cursor-pointer 
-              ${
-                selectedUserId === u.id
-                  ? "bg-transparent hover:bg-gray-300"
-                  : "bg-white hover:bg-gray-200"
+              ${selectedUserId === u.id
+                ? "bg-transparent hover:bg-gray-300"
+                : "bg-white hover:bg-gray-200"
               } shadow-none`}
             onClick={() => handleUserClick(u.id)}
           >
             <div className="flex gap-3">
-              <Avatar>
-                <AvatarImage src={u.avatar_url as string} />
-                <AvatarFallback>{u.full_name?.charAt(0)}</AvatarFallback>
-              </Avatar>
+              <div className="relative">
+                <Avatar >
+                  <AvatarImage src={u.avatar_url as string} />
+                  <AvatarFallback>{u.full_name?.charAt(0)}</AvatarFallback>
+                </Avatar>
+                {onlineUsers.includes(u.id) ? (
+
+                  <span className="size-3 absolute bottom-0 right-0 bg-green-500 rounded-full"></span>
+                ) : (<span className="size-3 absolute bottom-0 right-0 bg-yellow-500 rounded-full"></span>
+                )}
+              </div>
+
               <div>
                 <div className="text-sm font-semibold">{u.full_name}</div>
                 <div className="flex items-center">
-                <div className="text-xs line-clamp-1">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Autem, fugit.
+                  <div className="text-xs line-clamp-1">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Autem, fugit.
+                  </div>
+                  <div>
+                    <CheckCheck className="size-4 text-green-800" />
+                  </div>
                 </div>
-                <div>
-                  <CheckCheck className="size-4 text-green-800"/>
-                </div>
-                </div>
-                
+
               </div>
             </div>
           </Card>
         ))}
-  
-</ScrollArea>
 
-    
+      </ScrollArea>
+
+
     </div>
   );
 };
