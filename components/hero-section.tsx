418755b964ecb2bbb3a1e3bb8e459a18a60c3a1e
Faisal Husain <faisalhusain1320@gmail.com>
2025-05-21 10:33:29 +0530

    fix  : landing page fixes

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -4,20 +4,23 @@ import { ArrowRight } from 'lucide-react';
 
 export function HeroSection() {
   return (
-    <div className="relative min-h-[calc(100vh-66px)] w-full flex flex-col items-center justify-center bg-[#041200] text-white">
-    <div className="flex flex-col items-center justify-center ">
-      <h1 className="text-3xl md:text-5xl lg:text-7xl font-bold md:text-center mb-4 md:leading-24 text-left  text-balance">
+    <div className="relative md:h-[calc(100vh-66px)] w-full flex flex-col items-center justify-center bg-[#041200] text-white pt-5">
+    <div className="flex flex-col items-center justify-center p-3 ">
+        <div className='bg-[#ffffff1a] rounded-full px-4 py-1.5 mb-3 group'>
+          <p className="text-xs font-semibold text-white flex items-center">Periskope Launch Week 8 Is Here! <ArrowRight className='size-3 group-hover:translate-x-1 transition-transform duration-300'/></p>
+        </div>
+      <h1 className="text-4xl md:text-5xl lg:text-7xl font-bold md:text-center mb-4 md:leading-24 text-left  text-balance tracking-wider">
         Manage <span className="text-[#22c55e]">WhatsApp Groups</span><br />
         <span className="text-[#22c55e]">and Chats</span> at scale
       </h1>
-      <p className="text-sm md:text-xl text-gray-100 mt-2 mb-8 max-w-xl text-center">
+      <p className="text-sm md:text-xl text-gray-100 mt-2 mb-8 max-w-xl  md:text-center text-left">
         Connect multiple numbers, create tasks & tickets, integrate with your systems, and automate your workflows on WhatsApp
       </p>
-      <div className="flex gap-4 mb-8">
-        <Button className="bg-[#22c55e] hover:bg-[#22c55e]/90 text-white px-8 py-3 text-lg rounded-lg shadow-lg">
+      <div className="flex md:flex-row flex-col items-center justify-center gap-4 mb-8 w-full ">
+        <Button className="bg-[#15803d] hover:bg-[#15803d]/90 text-white font-medium rounded-md h-12 px-5 text-lg w-full md:w-auto">
           Sign Up for Free
         </Button>
-        <Button variant="outline" className="px-8 py-3 text-lg rounded-lg border-gray-400 text-white">
+        <Button variant={"outline"} className=" text-black font-medium rounded-md h-12 px-5 text-lg w-full md:w-auto">
           Book a Demo
         </Button>
       </div>
